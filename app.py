1415afec5bde296fff824d325473af94325145e6
Aurinko <jollot@yandex.ru>
2024-09-29 19:14:47 +0700

    Добавлен вывод информации о стоимости недвижимости

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    message = 'Test'
+
+    return render_template("index.html", message=message)
 
 
 if __name__ == "__main__":
