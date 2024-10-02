2ca0d58b823751b77082df9f6035c9949a230864
abeliks <abeliks@yandex.ru>
2024-10-02 16:41:14 +0300

    поменяли модель на random forest

--- processingS.py
+++ processingS.py
@@ -1,4 +1,3 @@
-# import numpy as np
 import pandas as pd
 import joblib
 import warnings
@@ -8,13 +7,13 @@ def process(data):
     data_df = pd.DataFrame([data])
 
     # Загружаем обученную модель
-    loaded_LR = joblib.load('LR_model.joblib')
+    loaded_LR = joblib.load('RF_model.joblib')
 
     # Предсказываем стоимость
     price = loaded_LR.predict(data_df).astype(int)
 
     # Убираем квадратные скобки и переводим стоимость в миллионы
-    price = price[0,0]  # результат будет одномерным массивом
+    price = price[0]  # результат будет одномерным массивом
     price = round(price / 1_000_000, 3)
 
     return price
