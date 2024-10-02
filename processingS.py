371fddd9342bf3209dad4b35213368c614fdbbd2
abeliks <abeliks@yandex.ru>
2024-10-02 12:21:09 +0300

    Навел порядок в processingS

--- processingS.py
+++ processingS.py
@@ -2,21 +2,12 @@ import pandas as pd
 import numpy as np
 import joblib
 
-# from sklearn import metrics
-# from sklearn import preprocessing
-# from sklearn.linear_model import LinearRegression #линейная регрессия
-
-
-
 def process(total_area, floor, min_to_metro, construction_year, number_of_rooms,ceiling_height):
 
-    # price = min_to_metro * floor * total_area * construction_year * number_of_rooms * ceiling_height
-    x = np.array([min_to_metro,total_area,floor,construction_year,ceiling_height,number_of_rooms])
-    x = x.reshape(1, -1)
+    # формируем вектор для predict
+    x = np.array([min_to_metro,total_area,floor,construction_year,ceiling_height,number_of_rooms]).reshape(1, -1)
 
-    # df_x = np.array([min_to_metro,total_area,floor,construction_year,ceiling_height,number_of_rooms]).reshape(-1,1)
-
-    # Load the saved model from a file
+    # Load the saved model from a file LR_model.joblib
     loaded_LR = joblib.load('LR_model.joblib')
 
     price  = loaded_LR.predict(x).astype(int)
