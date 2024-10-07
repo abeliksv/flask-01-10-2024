8d7a3640f9c189d13ff1893b57a65a960295e9dd
abeliks <abeliks@yandex.ru>
2024-10-07 12:04:44 +0300

    добавил проверку введенных значений в поля ввода

--- processingS.py
+++ processingS.py
@@ -3,7 +3,6 @@ import numpy as np
 import joblib
 
 
-
 def process(data):
     # Преобразуем словарь в DataFrame
     data_df = pd.DataFrame([data])
@@ -15,10 +14,13 @@ def process(data):
     # Предсказываем стоимость
     price = loaded_model.predict(data_df).astype(int)
 
-    # Убираем квадратные скобки и переводим стоимость в миллионы
-    price = price[0,0]  # результат будет одномерным массивом
-    # price = round(price / 1_000_000, 3)# применять при загрузке RF_model.joblib
-    price = np.round(price / 1_000_000, 3)
+    # Убираем квадратные скобки
+    price = price[0, 0]  # результат будет одномерным массивом
 
+    # применять при загрузке RF_model.joblib
+    # price = round(price / 1_000_000, 3)
+    # price = price[0]  # результат будет одномерным массивом
+    # преводим стоимость в млн. руб.
+    price = np.round(price / 1_000_000, 3)
 
     return price
