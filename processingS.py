b96a440710302bca9b31c4fa04bbe69c900be52b
abeliks <abeliks@yandex.ru>
2024-10-07 17:51:25 +0300

    улучшил читаемость кода

--- processingS.py
+++ processingS.py
@@ -1,9 +1,5 @@
 import pandas as pd
-# import numpy as np
 import joblib
-
-
-# import pickle
 
 
 def preprocess(data):
@@ -11,12 +7,11 @@ def preprocess(data):
         min_max_scaler_x = joblib.load('./models/min_max_scaler_x.joblib')
         print("Scaler_x loaded successfully.")
     except FileNotFoundError:
-        print("Error: Joblib file_x not found.")
+        print("Error: Joblib file min_max_scaler_x.joblib not found.")
         return None
     except Exception as e:
         print(f"An error_x occurred: {e}")
         return None
-
     scaled_data = min_max_scaler_x.transform(data)
     return scaled_data
 
@@ -26,26 +21,16 @@ def process(scaled_data):
         min_max_scaler_y = joblib.load('./models/min_max_scaler_y.joblib')
         print("Scaler_y loaded successfully.")
     except FileNotFoundError:
-        print("Error: Joblib file_y not found.")
+        print("Error: Joblib file min_max_scaler_y.joblib not found.")
         return None
     except Exception as e:
         print(f"An error_y occurred: {e}")
         return None
-
-    # Преобразуем словарь в DataFrame
+    # Преобразуем в DataFrame
     data_df = pd.DataFrame(scaled_data)
-
     # Загружаем обученную модель
-    # loaded_model = joblib.load('RF_model.joblib')
     loaded_model = joblib.load('models/LR_model_scaled.joblib')
-
     # Предсказываем стоимость
     scaled_price = loaded_model.predict(data_df)
-
-    price = min_max_scaler_y.inverse_transform([scaled_price])
-
-    # Убираем квадратные скобки
-    price = price[0, 0]
-
-
+    price = min_max_scaler_y.inverse_transform(scaled_price)[0][0]
     return price
